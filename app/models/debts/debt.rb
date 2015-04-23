a74d8d14622df115dd4fe46c45704b05361bfb23
Gary Tsai <gary.m.tsai@gmail.com>
2015-04-22 23:32:21 -0700

    adds javascript .onChange to caclulate the rate

--- app/models/debts/debt.rb
+++ app/models/debts/debt.rb
@@ -1,7 +1,15 @@
 class Debt < ActiveRecord::Base
 
+	before_save :set_rate
+
 	def self.types
 		['BankLoan', 'CreditCard', 'Mortgage', 'StudentLoan', 'Other']
+	end
+
+	private
+	
+	def set_rate
+		self.rate = monthly_payment / amount
 	end
 end
 

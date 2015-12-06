eb12678c9d119a5ae5fc47899c3b0c02078919ae
theInternauts <cricketw@wearetheinternauts.com>
2015-12-05 21:12:05 -0800

    changing color scheme for pie chart to be monochromatically split between each expense (this apprach has an uper limit-of usefulness beyond 10-15 expense items

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
 	def rand_hex_color
 		Array.new(3) {rand(255)}.inject('#') {|color, value| color + value.to_s(16)}.ljust(7,'f')
 	end
+
+	def spread_hex_color(index, total, color_max = 230)
+		# 0 == black, 255 == white
+		color_int = ((color_max / total) * index)
+		color_hex = color_int.to_s(16)
+		# output ex: '#efefef'
+		"#" + color_hex.ljust(6,color_hex)
+	end
 end
--- app/views/debts/_debts_chart.jbuilder
+++ app/views/debts/_debts_chart.jbuilder
@@ -1,6 +1,8 @@
+count = 1
 json.array! debts do |d|
 	json.value d.amount
-  json.color rand_hex_color()
+  json.color spread_hex_color(count, debts.length)
   json.highlight rand_hex_color()
   json.label d.type
+  count += 1
 end
